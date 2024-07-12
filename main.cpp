a24c7b6b5649de1bfa7705d030b3f39b9486ad5e
marisalieb <152454613+marisalieb@users.noreply.github.com>
2024-07-12 17:14:32 +0100

    add template

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <raylib.h>
+
+using namespace std;
+
+int main()
+{
+
+    cout << " started game ...";
+    InitWindow(750, 750, "Snake with a twist");
+
+    CloseWindow();
+
+    return 0;
+}
